fdba3ffab9c44b9fa3f22f1c1f8f4a2f578ca874
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-27 17:06:07 +0200

    added logic for Add and Update methods in RegionService

--- Thandizo.Core.BLL/Services/RegionService.cs
+++ Thandizo.Core.BLL/Services/RegionService.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Thandizo.ApiExtensions.DataMapping;
+using Thandizo.ApiExtensions.General;
 using Thandizo.DAL.Models;
 using Thandizo.DataModels.Core;
 using Thandizo.DataModels.General;
@@ -19,11 +22,13 @@ namespace Thandizo.Core.BLL.Services
         public async Task<OutputResponse> Get(int regionId)
         {
             var region = await _context.Regions.FirstOrDefaultAsync(x => x.RegionId.Equals(regionId));
+           
+            var mappedRegion = new AutoMapperHelper<Regions, RegionDTO>().MapToObject(region);
 
             return new OutputResponse
             {
                 IsErrorOccured = false,
-                Result = region
+                Result = mappedRegion
             };
         }
 
@@ -31,32 +36,66 @@ namespace Thandizo.Core.BLL.Services
         {
             var regions = await _context.Regions.OrderBy(x => x.RegionName).ToListAsync();
 
+            var mappedRegions = new AutoMapperHelper<Regions, RegionDTO>().MapToList(regions);
+
             return new OutputResponse
             {
                 IsErrorOccured = false,
-                Result = regions
+                Result = mappedRegions
             };
         }
 
         public async Task<OutputResponse> Add(RegionDTO region)
         {
-            var regions = await _context.Regions.OrderBy(x => x.RegionName).ToListAsync();
+            var isFound = await _context.Regions.AnyAsync(x => x.RegionName.ToLower() == region.RegionName.ToLower());
+            if (isFound)
+            {
+                return new OutputResponse
+                {
+                    IsErrorOccured = true,
+                    Message = "Region name already exist, duplicates not allowed"
+                };
+            }
+
+            var mappedRegion = new AutoMapperHelper<RegionDTO, Regions>().MapToObject(region);
+            mappedRegion.RowAction = "I";
+            mappedRegion.DateCreated = DateTime.UtcNow.AddHours(2);
+
+            await _context.Regions.AddAsync(mappedRegion);
+            await _context.SaveChangesAsync();
 
             return new OutputResponse
             {
                 IsErrorOccured = false,
-                Message = "The record has been created successfully"
+                Message = MessageHelper.AddNewSuccess
             };
         }
 
         public async Task<OutputResponse> Update(RegionDTO region)
         {
-            var regions = await _context.Regions.OrderBy(x => x.RegionName).ToListAsync();
+            var regionToUpdate = await _context.Regions.FirstOrDefaultAsync(x => x.RegionId.Equals(region.RegionId));
+
+            if (regionToUpdate == null)
+            {
+                return new OutputResponse
+                {
+                    IsErrorOccured = true,
+                    Message = "Region specified does not exist, update cancelled"
+                };
+            }
+
+            //update region details
+            regionToUpdate.RegionName = region.RegionName;
+            regionToUpdate.RowAction = "U";
+            regionToUpdate.ModifiedBy = region.CreatedBy;
+            regionToUpdate.DateModified = DateTime.UtcNow.AddHours(2);
+
+            await _context.SaveChangesAsync();
 
             return new OutputResponse
             {
                 IsErrorOccured = false,
-                Message = "The record has been updated successfully"
+                Message = MessageHelper.UpdateSuccess
             };
         }
 
@@ -80,7 +119,7 @@ namespace Thandizo.Core.BLL.Services
                 return new OutputResponse
                 {
                     IsErrorOccured = true,
-                    Message = "Region specified does not exist"
+                    Message = "Region specified does not exist, deletion cancelled"
                 };
             }
 
@@ -91,7 +130,7 @@ namespace Thandizo.Core.BLL.Services
             return new OutputResponse
             {
                 IsErrorOccured = false,
-                Message = "The record has been deleted successfully"
+                Message = MessageHelper.DeleteSuccess
             };
         }
     }
