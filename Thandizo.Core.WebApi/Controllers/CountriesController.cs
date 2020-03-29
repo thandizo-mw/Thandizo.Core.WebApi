1b67ca379d041909395e2bf7bb87b7c572181b38
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-29 16:30:04 +0200

    added NationalitiesController and CountriesController

---
+++ Thandizo.Core.WebApi/Controllers/CountriesController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Thandizo.ApiExtensions.Filters;
+using Thandizo.ApiExtensions.General;
+using Thandizo.Core.BLL.Services;
+using Thandizo.DataModels.Core;
+
+namespace Thandizo.Core.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        ICountryService _service;
+
+        public CountriesController(ICountryService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetByCode")]
+        [CatchException(MessageHelper.GetItemError)]
+        public async Task<IActionResult> GetByCode([FromQuery] string countryCode)
+        {
+            var response = await _service.Get(countryCode);
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpGet("GetAll")]
+        [CatchException(MessageHelper.GetListError)]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _service.Get();
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpPost("Add")]
+        [CatchException(MessageHelper.AddNewError)]
+        public async Task<IActionResult> Add([FromBody]CountryDTO country)
+        {
+            var outputHandler = await _service.Add(country);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler);
+            }
+
+            return Created("", outputHandler);
+        }
+
+        [HttpPut("Update")]
+        [CatchException(MessageHelper.UpdateError)]
+        public async Task<IActionResult> Update([FromBody]CountryDTO country)
+        {
+            var outputHandler = await _service.Update(country);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler);
+            }
+
+            return Ok(outputHandler);
+        }
+
+        [HttpDelete("Delete")]
+        [CatchException(MessageHelper.DeleteError)]
+        public async Task<IActionResult> Delete([FromQuery]string countryCode)
+        {
+            var outputHandler = await _service.Delete(countryCode);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler);
+            }
+
+            return Ok(outputHandler);
+        }
+    }
+}
