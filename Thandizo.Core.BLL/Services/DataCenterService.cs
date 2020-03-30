8c21dca4e85e98e61f2725da2206eb9372783eb7
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-03-30 16:21:09 +0200

    Added DataCenter Service classes

---
+++ Thandizo.Core.BLL/Services/DataCenterService.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Thandizo.ApiExtensions.DataMapping;
+using Thandizo.ApiExtensions.General;
+using Thandizo.DAL.Models;
+using Thandizo.DataModels.Core;
+using Thandizo.DataModels.DataCenters;
+using Thandizo.DataModels.General;
+
+namespace Thandizo.Core.BLL.Services
+{
+    public class DataCenterService : IDataCenterService
+    {
+        private readonly thandizoContext _context;
+
+        public DataCenterService(thandizoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<OutputResponse> Get(int centerId)
+        {
+            var DataCenter = await _context.DataCenters.FirstOrDefaultAsync(x => x.CenterId.Equals(centerId));
+           
+            var mappedDataCenter = new AutoMapperHelper<DataCenters, DataCenterDTO>().MapToObject(DataCenter);
+
+            return new OutputResponse
+            {
+                IsErrorOccured = false,
+                Result = mappedDataCenter
+            };
+        }
+
+        public async Task<OutputResponse> Get()
+        {
+            var dataCenters = await _context.DataCenters.OrderBy(x => x.CenterName).ToListAsync();
+
+            var mappedDataCenters = new AutoMapperHelper<DataCenters, DataCenterDTO>().MapToList(dataCenters);
+
+            return new OutputResponse
+            {
+                IsErrorOccured = false,
+                Result = mappedDataCenters
+            };
+        }
+
+        public async Task<OutputResponse> Add(DataCenterDTO dataCenter)
+        {
+            var isFound = await _context.DataCenters.AnyAsync(x => x.CenterName.ToLower() == dataCenter.CenterName.ToLower());
+            if (isFound)
+            {
+                return new OutputResponse
+                {
+                    IsErrorOccured = true,
+                    Message = "Data center name already exist, duplicates not allowed"
+                };
+            }
+
+            var mappedDataCenter = new AutoMapperHelper<DataCenterDTO, DataCenters>().MapToObject(dataCenter);
+            mappedDataCenter.RowAction = "I";
+            mappedDataCenter.DateCreated = DateTime.UtcNow.AddHours(2);
+
+            await _context.DataCenters.AddAsync(mappedDataCenter);
+            await _context.SaveChangesAsync();
+
+            return new OutputResponse
+            {
+                IsErrorOccured = false,
+                Message = MessageHelper.AddNewSuccess
+            };
+        }
+
+        public async Task<OutputResponse> Update(DataCenterDTO dataCenter)
+        {
+            var dataCenterToUpdate = await _context.DataCenters.FirstOrDefaultAsync(x => x.CenterId.Equals(dataCenter.CenterId));
+
+            if (dataCenterToUpdate == null)
+            {
+                return new OutputResponse
+                {
+                    IsErrorOccured = true,
+                    Message = "Data center specified does not exist, update cancelled"
+                };
+            }
+
+            //update data center details
+            dataCenterToUpdate.CenterName = dataCenter.CenterName;
+            dataCenterToUpdate.DistrictCode = dataCenter.DistrictCode;
+            dataCenterToUpdate.FacilityTypeId = dataCenter.FacilityTypeId;
+            dataCenterToUpdate.IsHealthFacility = dataCenter.IsHealthFacility;
+            dataCenterToUpdate.RowAction = "U";
+            dataCenterToUpdate.ModifiedBy = dataCenter.CreatedBy;
+            dataCenterToUpdate.DateModified = DateTime.UtcNow.AddHours(2);
+
+            await _context.SaveChangesAsync();
+
+            return new OutputResponse
+            {
+                IsErrorOccured = false,
+                Message = MessageHelper.UpdateSuccess
+            };
+        }
+
+        public async Task<OutputResponse> Delete(int centerId)
+        {
+            //check if there are any records associated with the specified Data center
+            var isFound = await _context.Patients.AnyAsync(x => x.DistrictCode.Equals(centerId));
+            if (isFound)
+            {
+                return new OutputResponse
+                {
+                    IsErrorOccured = true,
+                    Message = "The specified data center has patients attached, deletion denied"
+                };
+            }
+            else
+            {
+                isFound = await _context.HealthCareWorkers.AnyAsync(x => x.DataCenterId.Equals(centerId));
+                if (isFound)
+                {
+                    return new OutputResponse
+                    {
+                        IsErrorOccured = true,
+                        Message = "The specified data center has health care workers centers attached, deletion denied"
+                    };
+                }
+                else
+                {
+                    isFound = await _context.HealthCareWorkers.AnyAsync(x => x.DataCenterId.Equals(centerId));
+                    if (isFound)
+                    {
+                        return new OutputResponse
+                        {
+                            IsErrorOccured = true,
+                            Message = "The specified data center has health care workers attached, deletion denied"
+                        };
+                    }
+                    else
+                    { 
+                        if ((await _context.PatientFacilityMovements.AnyAsync(x => x.FromDataCenterId.Equals(centerId))) || (await _context.PatientFacilityMovements.AnyAsync(x => x.ToDataCenterId.Equals(centerId))))
+                        {
+                            return new OutputResponse
+                            {
+                                IsErrorOccured = true,
+                                Message = "The specified data center has patient facility movements attached, deletion denied"
+                            };
+                        }
+                    }
+                }
+            }
+
+
+            var DataCenter = await _context.DataCenters.FirstOrDefaultAsync(x => x.CenterId.Equals(centerId));
+
+            if (DataCenter == null)
+            {
+                return new OutputResponse
+                {
+                    IsErrorOccured = true,
+                    Message = "Data center specified does not exist, deletion cancelled"
+                };
+            }
+
+            //deletes the record permanently
+            _context.DataCenters.Remove(DataCenter);
+            await _context.SaveChangesAsync();
+
+            return new OutputResponse
+            {
+                IsErrorOccured = false,
+                Message = MessageHelper.DeleteSuccess
+            };
+        }
+    }
+}
---
+++ Thandizo.Core.BLL/Services/IDataCenterService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Thandizo.DataModels.DataCenters;
+using Thandizo.DataModels.General;
+
+namespace Thandizo.Core.BLL.Services
+{
+    public interface IDataCenterService
+    {
+        Task<OutputResponse> Add(DataCenterDTO dataCenter);
+        Task<OutputResponse> Delete(int centerId);
+        Task<OutputResponse> Get();
+        Task<OutputResponse> Get(int centerId);
+        Task<OutputResponse> Update(DataCenterDTO dataCenter);
+    }
+}
