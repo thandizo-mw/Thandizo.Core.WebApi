402e3515e14c40548e54cd64a0b0255a742c6085
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-03-30 16:23:20 +0200

    Added DistrictsController class

---
+++ Thandizo.Core.WebApi/Controllers/DistrictsController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Thandizo.ApiExtensions.Filters;
+using Thandizo.ApiExtensions.General;
+using Thandizo.Core.BLL.Services;
+using Thandizo.DataModels.Core;
+
+namespace Thandizo.Core.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DistrictsController : ControllerBase
+    {
+        IDistrictService _service;
+
+        public DistrictsController(IDistrictService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetById")]
+        [CatchException(MessageHelper.GetItemError)]
+        public async Task<IActionResult> GetById([FromQuery] string districtCode)
+        {
+            var response = await _service.Get(districtCode);
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpGet("GetAll")]
+        [CatchException(MessageHelper.GetListError)]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _service.Get();
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpPost("Add")]
+        [CatchException(MessageHelper.AddNewError)]
+        public async Task<IActionResult> Add([FromBody]DistrictDTO district)
+        {
+            var outputHandler = await _service.Add(district);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler);
+            }
+
+            return Created("", outputHandler);
+        }
+
+        [HttpPut("Update")]
+        [CatchException(MessageHelper.UpdateError)]
+        public async Task<IActionResult> Update([FromBody]DistrictDTO district)
+        {
+            var outputHandler = await _service.Update(district);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler);
+            }
+
+            return Ok(outputHandler);
+        }
+
+        [HttpDelete("Delete")]
+        [CatchException(MessageHelper.DeleteError)]
+        public async Task<IActionResult> Delete([FromQuery]string districtCode)
+        {
+            var outputHandler = await _service.Delete(districtCode);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler);
+            }
+
+            return Ok(outputHandler);
+        }
+    }
+}
