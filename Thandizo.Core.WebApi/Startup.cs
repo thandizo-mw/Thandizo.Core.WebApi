c8d5da608a685280250b03addc60485fc33923bd
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-31 14:59:46 +0200

    added middleware in Startup class to isable automatic model state validation to provide cleaner error messages to avoid default complex object

--- Thandizo.Core.WebApi/Startup.cs
+++ Thandizo.Core.WebApi/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,16 +18,20 @@ namespace Thandizo.Core.WebApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddEntityFrameworkNpgsql().AddDbContext<thandizoContext>(options =>
                         options.UseNpgsql(Configuration.GetConnectionString("DatabaseConnection")));
             services.AddDomainServices();
+
+            //Disable automatic model state validation to provide cleaner error messages to avoid default complex object
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
