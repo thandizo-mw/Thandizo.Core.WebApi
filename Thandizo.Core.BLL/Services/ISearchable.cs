97e92f199e2a7767207520c97e05e6026beb1691
Henry Alfred Sauzande <henry.sauzande@angledimension.com>
2020-04-05 23:46:04 +0200

    Added ISearchable interface

---
+++ Thandizo.Core.BLL/Services/ISearchable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Thandizo.DataModels.General;
+
+namespace Thandizo.Core.BLL.Services
+{
+    public interface ISearchable
+    {
+        Task<OutputResponse> Search(string searchText);
+    }
+}
