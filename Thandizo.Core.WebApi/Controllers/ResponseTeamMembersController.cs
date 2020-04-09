07873cd6b759492b102d20eac0b2e9298b1c739e
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-04-09 15:45:12 +0200

    added ResponseTeamMappingsController and ResponseTeamMembersController

---
+++ Thandizo.Core.WebApi/Controllers/ResponseTeamMembersController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Thandizo.ApiExtensions.Filters;
+using Thandizo.ApiExtensions.General;
+using Thandizo.Core.BLL.Services;
+using Thandizo.DataModels.Core;
+
+namespace Thandizo.Core.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResponseTeamMembersController : ControllerBase
+    {
+        IResponseTeamMemberService _service;
+
+        public ResponseTeamMembersController(IResponseTeamMemberService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetById")]
+        [CatchException(MessageHelper.GetItemError)]
+        public async Task<IActionResult> GetById([FromQuery] int teamMemberId)
+        {
+            var response = await _service.Get(teamMemberId);
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpGet("GetAll")]
+        [CatchException(MessageHelper.GetListError)]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _service.Get();
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpPost("Add")]
+        [ValidateModelState]
+        [CatchException(MessageHelper.AddNewError)]
+        public async Task<IActionResult> Add([FromBody]ResponseTeamMemberDTO teamMember)
+        {
+            var outputHandler = await _service.Add(teamMember);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler.Message);
+            }
+
+            return Created("", outputHandler.Message);
+        }
+
+        [HttpPut("Update")]
+        [ValidateModelState]
+        [CatchException(MessageHelper.UpdateError)]
+        public async Task<IActionResult> Update([FromBody]ResponseTeamMemberDTO teamMember)
+        {
+            var outputHandler = await _service.Update(teamMember);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler.Message);
+            }
+
+            return Ok(outputHandler.Message);
+        }
+
+        [HttpDelete("Delete")]
+        [CatchException(MessageHelper.DeleteError)]
+        public async Task<IActionResult> Delete([FromQuery]int teamMemberId)
+        {
+            var outputHandler = await _service.Delete(teamMemberId);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler.Message);
+            }
+
+            return Ok(outputHandler.Message);
+        }
+    }
+}
